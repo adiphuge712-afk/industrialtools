4c9af51a390406bc8cb246ce1b2211391a7e9c92
Aditya_Phuge <adiphuge712@gmail.com>
2025-12-24 13:21:49 +0530

    modified file

--- javagit_bypratikshamam/src/javagit_bypratikshamam/simple_program.java
+++ javagit_bypratikshamam/src/javagit_bypratikshamam/simple_program.java
@@ -2,6 +2,6 @@ package javagit_bypratikshamam;
 
 public class simple_program {
 public static void main(String[] args) {
-	System.out.println("addition is"+(2+3));
+	System.out.println("addition is"+(5+3));
 }
 }
