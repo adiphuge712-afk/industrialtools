97135d878c7d2469b5a035c8a5a4226598550f5c
Aditya_Phuge <adiphuge712@gmail.com>
2025-12-24 12:49:42 +0530

    addition moditied

--- javagit_bypratikshamam/src/javagit_bypratikshamam/simple_program.java
+++ javagit_bypratikshamam/src/javagit_bypratikshamam/simple_program.java
@@ -2,6 +2,6 @@ package javagit_bypratikshamam;
 
 public class simple_program {
 public static void main(String[] args) {
-	System.out.println("hello world");
+	System.out.println("addition is"+(2+3));
 }
 }
