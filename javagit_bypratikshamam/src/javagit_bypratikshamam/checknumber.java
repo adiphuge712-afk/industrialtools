b052654a7f2c1976e962bc89ed7831152f0105bb
Aditya_Phuge <adiphuge712@gmail.com>
2026-01-01 13:36:49 +0530

    checknuber is add

---
+++ javagit_bypratikshamam/src/javagit_bypratikshamam/checknumber.java
@@ -0,0 +1,12 @@
+package javagit_bypratikshamam;
+
+public class checknumber {
+	public static void main(String[] args) {
+		int a=2;
+		if(a%2==0) {
+			System.out.println("number is even");
+		}else {
+			System.out.println("number is odd");
+		}
+	}
+}
